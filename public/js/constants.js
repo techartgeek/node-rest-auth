8d01b88f136f5f563906a5903baf43df25fa4d72
techartgeek <vikasharma150880@gmail.com>
2016-09-02 02:57:32 +0530

    .

---
+++ public/js/constants.js
@@ -0,0 +1,7 @@
+angular.module('starter')
+    .constant('AUTH_EVENTS', {
+        notAuthenticated: 'auth-not-authenticated'
+    })
+    .constant('API_ENDPOINT', {
+        url: 'http://localhost:3000/api'
+    });
