d1bfd417299c622e7c417c33e131bad5e8c00e73
techartgeek <vikasharma150880@gmail.com>
2016-09-02 02:17:06 +0530

    .

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/bower_components/'));
 
 app.use(passport.initialize());
 
