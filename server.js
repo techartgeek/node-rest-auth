25a8597df023c1d26903239181b14f2c124a43aa
techartgeek <vikasharma150880@gmail.com>
2016-09-02 01:40:52 +0530

    .

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public/'));
 
 app.use(passport.initialize());
 
