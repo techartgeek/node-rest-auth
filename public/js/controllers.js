d2ed9de2befb8850ed11108d8448f87946d78507
Vikas Sharma <viksharm@adobe.com>
2016-09-02 15:15:06 +0530

    .

---
+++ public/js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('starter')
+    .controller('LoginCtrl', function ($scope, AuthService, $state) {
+
+    })
+    .controller('RegisterCtrl', function ($scope, AuthService, $state) {
+
+    })
+    .controller('InsideCtrl', function ($scope, AuthService, $state) {
+
+    })
+    .controller('AppCtrl', function ($scope, AuthService, $state) {
+
+    });
--- public/js/services.js
+++ public/js/services.js
@@ -1,89 +1,86 @@
 angular.module('starter')
+    .service('AuthService', function ($q, $http, API_ENDPOINT) {
+        var LOCAL_TOKEN_KEY = 'yourTokenKey';
+        var isAuthenticated = false;
+        var authToken;
 
-.service('AuthService', function ($q, $http, API_ENDPOINT) {
-    var LOCAL_TOKEN_KEY = 'yourTokenKey';
-    var isAuthenticated = false;
-    var authToken;
+        function loadUserCredentials() {
+            var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
+            if (token) {
+                useCredentials(token);
+            }
+        }
 
-    function loadUserCredentials() {
-        var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
-        if(token){
+        function storeUserCredentials(token) {
+            window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
             useCredentials(token);
         }
-    }
 
-    function storeUserCredentials(token) {
-        window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
-        useCredentials(token);
-    }
+        function useCredentials(token) {
+            isAuthenticated = true;
+            authToken = token;
 
-    function useCredentials(token) {
-        isAuthenticated = true;
-        authToken = token;
-
-        $http.defaults.headers.common.Authorization = authToken;
-    }
-
-    function destroyUserCredentials() {
-        authToken = undefined;
-        isAuthenticated = false;
-        $http.defaults.headers.common.Authorization = undefined;
-        window.localStorage.removeItem(LOCAL_TOKEN_KEY);
-    }
-
-    var register = function (user) {
-        return $q(function (resolve, reject) {
-            $http.post(API_ENDPOINT.url + '/signup', user).then(function (result) {
-                if(result.data.success){
-                    resolve(result.data.msg);
-                } else {
-                    reject(result.data.msg);
-                }
-            });
-        });
-    };
-
-    var login = function (user) {
-        return $q(function (resolve, reject) {
-            $http.post(API_ENDPOINT.url + '/authenticate', user).then(function (result) {
-                if(result.data.success){
-                    storeUserCredentials(result.data.token);
-                    resolve(result.data.msg);
-                } else {
-                    reject(result.data.msg);
-                }
-            });
-        });
-    };
-
-    var logout = function () {
-        destroyUserCredentials();
-    };
-
-    loadUserCredentials();
-
-    return {
-        login: login,
-        register: register,
-        logout: logout,
-        isAuthenticated: function () {
-            return isAuthenticated;
+            $http.defaults.headers.common.Authorization = authToken;
         }
-    };
 
-})
-
-.factory('AuthInterceptor', function ($rootScope, $q, AUTH_EVENTS) {
-    return {
-        responseError: function (response) {
-            $rootScope.$broadcast({
-                401: AUTH_EVENTS.notAuthenticated
-            }[response.status], response);
-            return $q.reject(response);
+        function destroyUserCredentials() {
+            authToken = undefined;
+            isAuthenticated = false;
+            $http.defaults.headers.common.Authorization = undefined;
+            window.localStorage.removeItem(LOCAL_TOKEN_KEY);
         }
-    };
-})
 
-.config(function ($httpProvider) {
-    $httpProvider.interceptors.push('AuthInterceptor');
-});
+        var register = function (user) {
+            return $q(function (resolve, reject) {
+                $http.post(API_ENDPOINT.url + '/signup', user).then(function (result) {
+                    if (result.data.success) {
+                        resolve(result.data.msg);
+                    } else {
+                        reject(result.data.msg);
+                    }
+                });
+            });
+        };
+
+        var login = function (user) {
+            return $q(function (resolve, reject) {
+                $http.post(API_ENDPOINT.url + '/authenticate', user).then(function (result) {
+                    if (result.data.success) {
+                        storeUserCredentials(result.data.token);
+                        resolve(result.data.msg);
+                    } else {
+                        reject(result.data.msg);
+                    }
+                });
+            });
+        };
+
+        var logout = function () {
+            destroyUserCredentials();
+        };
+
+        loadUserCredentials();
+
+        return {
+            login: login,
+            register: register,
+            logout: logout,
+            isAuthenticated: function () {
+                return isAuthenticated;
+            }
+        };
+
+    })
+    .factory('AuthInterceptor', function ($rootScope, $q, AUTH_EVENTS) {
+        return {
+            responseError: function (response) {
+                $rootScope.$broadcast({
+                    401: AUTH_EVENTS.notAuthenticated
+                }[response.status], response);
+                return $q.reject(response);
+            }
+        };
+    })
+    .config(function ($httpProvider) {
+        $httpProvider.interceptors.push('AuthInterceptor');
+    });
